e1968fa956997a6426cf207bb5b1faa77a863312
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-01-09 21:39:54 -0800

    Add better formating of content

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,30 @@ import { token } from '../config.json';
 import channels from '../channels.json';
 import axios from 'axios';
 import fs from 'node:fs';
+import TurndownService from 'turndown';
 
 const client = new Client({
   intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'],
 });
+const turndownService = new TurndownService();
 
 process.on('unhandledRejection', console.error);
 (async () => {
   client.on('ready', () => {
     console.log('Ready as ' + client.user.tag);
+    client.user.setActivity({
+      type: 'WATCHING',
+      name: 'the LWSD homepage',
+    });
   });
   setInterval(async () => {
     const homePage = await axios.get('https://lwsd.org');
     const $ = load(homePage.data);
     const cache = JSON.parse(fs.readFileSync('./cache.json').toString());
     if ($('.fsPagePopTitle').html() !== cache.lastEntry) {
-      const pageContent = $('.fsPagePopMessage')
-        .html()
-        .replaceAll(/<p>/gi, '')
-        .replaceAll(/<\/p>/gi, '\n')
+      const pageContent = turndownService
+        .turndown($('.fsPagePopMessage').html())
         .replaceAll(/\n\n/gi, '\n')
-        .replaceAll(
-          /<a\shref=\"([^"]*)">([^<]*)<\/a>/gim,
-          (match, url, text) => '[' + text + '](' + url + ')'
-        )
         .replaceAll('&nbsp;', '\n');
       channels.forEach((channel) => {
         (client.channels.cache.get(channel) as TextChannel)?.send({
