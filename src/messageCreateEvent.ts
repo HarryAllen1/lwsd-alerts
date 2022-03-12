d35cdfb6006850098fa73d13e4d5c4725a257686
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-03-12 12:22:32 -0800

    >:)

--- src/messageCreateEvent.ts
+++ src/messageCreateEvent.ts
@@ -1,12 +1,105 @@
 import axios from 'axios';
 import { Client, Message } from 'discord.js';
-
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 export default async function MessageCreateEvent(
   client: Client,
   message: Message
 ) {
   if (message.author.bot) return;
 
+  if (
+    (message.author.id === '714640925703274547' &&
+      message.content.startsWith('pls rob')) ||
+    message.content.startsWith('pls steal')
+  ) {
+    if (/[harry potter|696554549418262548]/gi.test(message.content)) {
+      message.reply('wtf. you now get spam');
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+      message.author.send('hello');
+      await sleep(5000);
+      message.channel.send(message.author.toString()).then(async (msg) => {
+        await sleep(1000);
+        msg.delete();
+      });
+      await sleep(5000);
+    }
+  }
   if (!message.content.startsWith('$')) return;
   const [cmdName, ...args] = message.content
     .slice('$'.length)
