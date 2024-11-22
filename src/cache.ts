96ed95c0de853e5d9a044cf2d280bae167e9cdd6
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-11-21 20:31:30 -0800

    fix again

--- src/cache.ts
+++ src/cache.ts
@@ -1,12 +1,15 @@
 import { readFile, writeFile } from 'node:fs/promises';
 import { CacheEntry } from './types.js';
+import { existsSync } from 'node:fs';
 
 export const writeToCache = async (content: string): Promise<void> =>
   await writeFile('./cache.json', content);
 
 export const readCache = async (): Promise<{ lastEntries: CacheEntry[] }> =>
   JSON.parse(
-    (await readFile('./cache.json', 'utf8')) || '{"lastEntries": []}'
+    existsSync('./cache.json')
+      ? await readFile('./cache.json', 'utf8')
+      : '{"lastEntries": []}'
   ) as {
     lastEntries: CacheEntry[];
   };
